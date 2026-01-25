af44c5d903f64c9658c46d4748e5223d372cfa10
Burak Altunoluk <63430143+BurakAltunoluk@users.noreply.github.com>
2026-01-25 21:40:42 +0000

    Create temiz_sunucu_kodu.ino

---
+++ temiz_sunucu_kodu.ino
@@ -0,0 +1,38 @@
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
+int menuGonderildi = 0;
+
+
+void setup() {
+ Serial.begin(115200);
+ delay(1000);
+ SerialBT.begin("ESP32_SUNUCU");
+}
+
+
+
+void loop() {
+    /* ---- CLIENT'tan gelenler ---- */
+  if (SerialBT.available()) {
+    String gelen = SerialBT.readStringUntil('\n');
+    gelen.trim();
+    Serial.println(gelen);
+
+  }
+ if (SerialBT.hasClient() && menuGonderildi == 0) {
+    menuGonderildi = 1;
+    drawMainUI();
+  }
+}
+
+
+void drawMainUI() {
+  SerialBT.println("rotation(0)");
+  delay(150);
+
+  // index 0 - Baslik
+  SerialBT.println("clear(0xCD0E0E)");
+  delay(80);
+  SerialBT.println("button(35,157,160,48,0x7C5CFF,0xFFFFFF,send,CMD,1)");
+
+}
